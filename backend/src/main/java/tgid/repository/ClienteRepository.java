9e353766d755fb21ca5d7115d4f943a19e9195e9
Felipe Sobral <felipesobral_@hotmail.com>
2024-02-08 11:54:26 -0300

    Ajuste do ClienteRepository

--- backend/src/main/java/tgid/repository/ClienteRepository.java
+++ backend/src/main/java/tgid/repository/ClienteRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tgid.entity.Cliente;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
